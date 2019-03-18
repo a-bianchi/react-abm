889d203f6279c9a7fbba83fcafa4b2ae557ae1e1
Alejo <alejobianchi@gmail.com>
2019-03-17 21:21:53 -0300

    UPDATE_CUSTOMER state

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -4,6 +4,21 @@ import { FETCH_CUSTOMERS, INSERT_CUSTOMER, UPDATE_CUSTOMER } from '../constants'
 export const customers = handleActions({
     [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
     [INSERT_CUSTOMER]: (state, action) => [...state, action.payload],
-    //[UPDATE_CUSTOMER]: (state, action) => [...state, action.payload],
+    [UPDATE_CUSTOMER]: (state, action) => {
+        const customerPayload = action.payload;
+        const { id } = customerPayload;
+        
+        const customers = state;
+        const initialValue = [];
+        const newCustomer = customers.reduce((acc, customer) => {
+            if(customer.id === id){
+                return [...acc, customerPayload];
+            } else {
+                return [...acc, customer]
+            }
+        }, initialValue);
+
+        return newCustomer;
+    },
 }, []);
 
